d0e0305851dda05216c35070cc3d5db4afd02a8b
diegowanborges <diegowanborges@gmail.com>
2021-05-12 16:37:40 -0300

    Fix Product

--- front-web/src/pages/Registrations/Components/Product/Form/index.tsx
+++ front-web/src/pages/Registrations/Components/Product/Form/index.tsx
@@ -12,7 +12,7 @@ type ParamsType = {
     productId: string;
 }
 function ProductForm() {
-    const { register, handleSubmit, errors, setValue, getValues, control } = useForm<Product>();
+    const { register, handleSubmit, errors, setValue, control } = useForm<Product>();
     const history = useHistory();
     const { productId } = useParams<ParamsType>();
     const isEditing = productId !== 'new';
@@ -49,6 +49,18 @@ function ProductForm() {
             makePrivateRequest({ url: `/products/${productId}` })
                 .then(response => {
                     setValue('name', response.data.name);
+                    setValue('description', response.data.description);
+                    setValue('packaging', response.data.packaging);
+                    setValue('netWeight', response.data.netWeight);
+                    setValue('grossWeight', response.data.grossWeight);
+                    setValue('validityDays', response.data.validityDays);
+                    setValue('rawMaterialConversion', response.data.rawMaterialConversion);
+                    setValue('costLastEntry', response.data.costLastEntry);
+                    setValue('averageCost', response.data.averageCost);
+                    setValue('stockBalance', response.data.stockBalance);
+                    setValue('unity', response.data.unity);
+                    setValue('group', response.data.group);
+
                     
                 })
         }
@@ -75,10 +87,11 @@ function ProductForm() {
         history.push("./")
     }
     return (
-        <form onSubmit={handleSubmit(onSubmit)} className="user-form">
-            <div className="user-form-main">
-                <div className="user-form-name">
-                    <label className="label-base" >Nome Completo:</label>
+        <form onSubmit={handleSubmit(onSubmit)} className="product-form">
+          
+            <div className="product-form-row">
+                <div className="product-form-name">
+                    <label className="label-base" >Nome do Produto:</label>
                     <input
                         name="name"
                         type="text"
@@ -86,7 +99,7 @@ function ProductForm() {
                         ref={register({
                             required: "Campo obrigatório",
                             minLength: { value: 2, message: "O campo deve ter minímo 2 caracteres" },
-                            maxLength: { value: 50, message: "O campo deve ter no maximo 10 caracteres" },
+                            maxLength: { value: 40, message: "O campo deve ter no maximo 10 caracteres" },
                         })}
                     />
                     {errors.name && (
@@ -95,8 +108,9 @@ function ProductForm() {
                         </div>
                     )}
                 </div>
-                <div className="user-form-email">
-                    <label className="label-base">E-mail:</label>
+
+                <div className="product-form-description">
+                    <label className="label-base">Descrição:</label>
                     <input
                         type="teste"
                         name="description"
@@ -113,9 +127,133 @@ function ProductForm() {
                         </div>
                     )}
                 </div>
-
             </div>
-            <div className="user-form-select">
+
+            <div className="product-form-row">
+                <div className="product-form-row-item">
+                    <label className="label-base" >Embalagem:</label>
+                    <input
+                        name="packaging"
+                        type="text"
+                        className="input-base"
+                        ref={register({
+                            required: "Campo obrigatório",
+                            minLength: { value: 2, message: "O campo deve ter minímo 2 caracteres" },
+                            maxLength: { value: 40, message: "O campo deve ter no maximo 10 caracteres" },
+                        })}
+                    />
+                    {errors.packaging && (
+                        <div className="invalid-feedback d-block">
+                            {errors.packaging.message}
+                        </div>
+                    )}
+                </div>
+                <div className="product-form-row-item">
+                    <label className="label-base">Peso Liquido:</label>
+                    <input
+                        type="number"
+                        step="0.001"
+                        name="netWeight"
+                        className="input-base"
+                        ref={register({
+                            required: "Campo obrigatório"
+                        })}
+                    />
+                    {errors.netWeight && (
+                        <div className="invalid-feedback d-block">
+                            {errors.netWeight?.message}
+                        </div>
+                    )}
+                </div>
+                <div className="product-form-row-item">
+                    <label className="label-base">Peso Bruto:</label>
+                    <input
+                        type="number"
+                        step="0.001"
+                        name="grossWeight"
+                        className="input-base"
+                        ref={register({
+                            required: "Campo obrigatório"
+                        })}
+                    />
+                    {errors.grossWeight && (
+                        <div className="invalid-feedback d-block">
+                            {errors.grossWeight?.message}
+                        </div>
+                    )}
+                </div>
+                <div className="product-form-row-item">
+                    <label className="label-base">Validade(Dias):</label>
+                    <input
+                        type="number"
+                        name="validityDays"
+                        className="input-base"
+                        ref={register({
+                            required: "Campo obrigatório"
+                        })}
+                    />
+                    {errors.validityDays && (
+                        <div className="invalid-feedback d-block">
+                            {errors.validityDays?.message}
+                        </div>
+                    )}
+                </div>
+            </div>
+        
+            <div className="product-form-row">
+               
+                <div className="product-form-row-item">
+                    <label className="label-base" >Conver.materia prima:</label>
+                    <input
+                        name="rawMaterialConversion"
+                        type="text"
+                        className="input-base"
+                        ref={register({
+                            required: "Campo obrigatório"
+                        })}
+                    />
+                    {errors.rawMaterialConversion && (
+                        <div className="invalid-feedback d-block">
+                            {errors.rawMaterialConversion.message}
+                        </div>
+                    )}
+                </div>
+
+                <div className="product-form-row-item">
+                    <label className="label-base">Custo Ult. Ent.:</label>
+                    <input
+                        type="number"
+                        name="costLastEntry"
+                        className="input-base"
+                        ref={register}
+                        readOnly
+                    />
+                </div>
+
+                <div className="product-form-row-item">
+                    <label className="label-base">Custo Médio:</label>
+                    <input
+                        type="number"
+                        name="averageCost"
+                        className="input-base"
+                        ref={register}
+                        readOnly
+                    />
+                </div>
+
+                <div className="product-form-row-item">
+                    <label className="label-base">Saldo Estoque:</label>
+                    <input
+                        type="number"
+                        name="stockBalance"
+                        className="input-base"
+                        ref={register}
+                        readOnly
+                    />
+                </div>
+            </div>
+
+            <div className="product-form-select">
                 <label className="label-base">Selecione o grupo:</label>
                 <Controller
                     as={Select}
@@ -136,7 +274,7 @@ function ProductForm() {
                 )}
             </div>
 
-            <div className="user-form-select">
+            <div className="product-form-select">
                 <label className="label-base">Selecione a unidade:</label>
                 <Controller
                     as={Select}
@@ -157,16 +295,14 @@ function ProductForm() {
                 )}
             </div>
 
-            
-
-            <div className="user-form-actions">
+            <div className="product-form-actions">
                 <button
                     className="btn btn-outline-danger"
                     onClick={handleCancel}
                 >
                     CANCELAR
                 </button>
-                <button className="btn btn-primary user-form-btn-save">
+                <button className="btn btn-primary product-form-btn-save">
                     SALVAR
                 </button>
             </div>
