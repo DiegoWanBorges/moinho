59f3e0a3792586d626279f24d9bb02e714504795
diegowanborges <diegowanborges@gmail.com>
2021-07-07 14:24:51 -0300

    Added Loading in login

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./app.scss"
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "react-datetime/css/react-datetime.css";
--- front-web/src/pages/Login/LoginCard/index.tsx
+++ front-web/src/pages/Login/LoginCard/index.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useHistory, useLocation } from 'react-router';
 import './styles.scss';
+import Loader from "react-loader-spinner";
 type FormState = {
     username: string;
     password: string;
@@ -15,10 +16,12 @@ function LoginCard() {
     const { register, handleSubmit, errors } = useForm<FormState>();
     const [hasError, setHasError] = useState(false);
     const history = useHistory();
+    const [isLoading, setIsLoading] = useState(false);
     const location = useLocation<LocationState>();
     let { from } = location.state || { from: { pathname: "/home" } };
 
     const onSubmit = (data: FormState) => {
+        setIsLoading(true)
         makeLogin(data)
             .then(response => {
                 setHasError(false);
@@ -28,6 +31,9 @@ function LoginCard() {
             .catch(() => {
                 setHasError(true);
             })
+            .finally(() => {
+                setIsLoading(false)
+            })
 
             ;
     }
@@ -35,47 +41,60 @@ function LoginCard() {
         <form className="login-card-main" onSubmit={handleSubmit(onSubmit)}>
             <h1 className="login-card-title">
                 Informe suas credenciais:
-           </h1>
-           {hasError && (
-                    <div className="alert alert-danger">
-                        Usuário ou senha inválido!
-                    </div>
-            )}
-            <input
-                className="login-card-input"
-                type="email"
-                placeholder="Email"
-                name="username"
-                ref={register({
-                    required: "Campo obrigatório",
-                    pattern: {
-                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                        message: "Email inválido"
-                    }
-                })}
-            />
-            {errors.username && (
-                <div className="invalid-feedback d-block">
-                    {errors.username.message}
-                </div>
-            )}
-            <input
-                className="login-card-input login-card-input-pwd"
-                type="password"
-                placeholder="Senha"
-                name="password"
-                ref={register({ required: "Campo obrigatório" })}
-            />
-            {errors.password && (
-                <div className="invalid-feedback d-block">
-                    {errors.password.message}
-                </div>
-            )}
-            <button
-                className="login-card-btn"
-            >
-                LOGAR
-            </button>
+            </h1>
+            {
+                isLoading ? <Loader
+                    type="Oval"
+                    height={100}
+                    width={100}
+                    color="#0670B8"
+                /> :
+                    (
+                        <>
+                            {hasError && (
+                                <div className="alert alert-danger">
+                                    Usuário ou senha inválido!
+                                </div>
+                            )}
+                            <input
+                                className="login-card-input"
+                                type="email"
+                                placeholder="Email"
+                                name="username"
+                                ref={register({
+                                    required: "Campo obrigatório",
+                                    pattern: {
+                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                        message: "Email inválido"
+                                    }
+                                })}
+                            />
+                            {errors.username && (
+                                <div className="invalid-feedback d-block">
+                                    {errors.username.message}
+                                </div>
+                            )}
+                            <input
+                                className="login-card-input login-card-input-pwd"
+                                type="password"
+                                placeholder="Senha"
+                                name="password"
+                                ref={register({ required: "Campo obrigatório" })}
+                            />
+                            {errors.password && (
+                                <div className="invalid-feedback d-block">
+                                    {errors.password.message}
+                                </div>
+                            )}
+                            <button
+                                className="login-card-btn"
+                            >
+                                LOGAR
+                            </button>
+                        </>
+                    )
+            }
+
         </form>
     );
 }
